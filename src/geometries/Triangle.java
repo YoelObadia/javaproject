a91153415c05aafe2bf76fff3cecda82688989d5
YoelObadia <73312969+YoelObadia@users.noreply.github.com>
2021-03-18 23:37:01 +0200

    Update Triangle.java

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -3,6 +3,10 @@ package geometries;
 import primitives.Point3D;
 import primitives.Vector;
 
+/**
+ *@author Yoel and Jerry
+*/
+
 public class Triangle extends Polygon{
 
     public Triangle(Point3D vertices) {
